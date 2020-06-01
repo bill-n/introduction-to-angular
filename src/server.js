493efda96caa4fcb36fe56326887a5abecae8a34
bill <bill-n@teebill15@gmail.com>
2020-06-01 13:02:34 +0000

    modified

--- src/server.js
+++ src/server.js
@@ -1,30 +1,59 @@
-//Install express server
-const express = require("express");
-const path = require("path");
-const cookieSession = require("cookie-session");
-const cookieParser = require("cookie-parser");
-
-const app = express();
-
-app.use(cookieParser());
-
-app.use(
-  cookieSession({
-    name: "session",
-    keys: [process.env.SECRET],
-    maxAge: 2 * 24 * 60 * 1000
-  })
-);
-
-app.use(express.static(__dirname + "/dist/introduction-to-angular/"));
-
-app.get("/*", function(req, res) {
-//   res.cookie("holiday_request_service_url", process.env.HOLIDAY_REQUESTS_SERVICE_URL);
-//   res.cookie("token_request_url", process.env.TOKEN_REQUEST_URL);
-//   res.cookie("oidc_client_id", process.env.OIDC_CLIENT_ID);
-//   res.cookie("redirect_page_to_oidc", process.env.REDIRECT_PAGE_TO_OIDC);
-
-  res.sendFile(path.join(__dirname + "/introduction-to-angular/index.html"));
-});
-
-app.listen(process.env.PORT || 8080);
+{
+    "name": "introduction-to-angular",
+    "version": "0.0.0",
+    "license": "MIT",
+    "scripts": {
+      "ng": "ng",
+      "start": "node server.js",
+      "build": "ng build",
+      "test": "ng test",
+      "lint": "ng lint",
+      "e2e": "ng e2e",
+      "postinstall": "ng build --aot -prod"
+    },
+    "private": true,
+    "dependencies": {
+      "@angular/animations": "^4.2.4",
+      "@angular/cli": "^1.4.9",
+      "@angular/common": "^4.2.4",
+      "@angular/compiler": "^4.2.4",
+      "@angular/compiler-cli": "^4.4.6",
+      "@angular/core": "^4.2.4",
+      "@angular/forms": "^4.2.4",
+      "@angular/http": "^4.2.4",
+      "@angular/platform-browser": "^4.2.4",
+      "@angular/platform-browser-dynamic": "^4.2.4",
+      "@angular/router": "^4.2.4",
+      "core-js": "^2.4.1",
+      "express": "^4.16.2",
+      "rxjs": "^5.4.2",
+      "typescript": "~2.3.3",
+      "zone.js": "^0.8.14"
+    },
+    "devDependencies": {
+      "@angular/cli": "^1.4.9",
+      "@angular/compiler-cli": "^4.4.6",
+      "@angular/language-service": "^4.2.4",
+      "@types/jasmine": "~2.5.53",
+      "@types/jasminewd2": "~2.0.2",
+      "@types/node": "~6.0.60",
+      "codelyzer": "~3.2.0",
+      "enhanced-resolve": "^3.3.0",
+      "jasmine-core": "~2.6.2",
+      "jasmine-spec-reporter": "~4.1.0",
+      "karma": "~1.7.0",
+      "karma-chrome-launcher": "~2.1.1",
+      "karma-cli": "~1.0.1",
+      "karma-coverage-istanbul-reporter": "^1.2.1",
+      "karma-jasmine": "~1.1.0",
+      "karma-jasmine-html-reporter": "^0.2.2",
+      "protractor": "~5.1.2",
+      "ts-node": "~3.2.0",
+      "tslint": "~5.7.0",
+      "typescript": "~2.3.3"
+    },
+    "engines": {
+      "node": "6.11.0",
+      "npm": "3.10.10"
+    }
+  }
